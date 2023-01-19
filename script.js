63bd65d345294f824f328ca0a2fd2b8d888997e3
Sara Gilani <saragilani@Saras-MacBook-Pro.local>
2023-01-19 03:09:44 +0000

    search function completed with added input and search button. returns json of first country

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("HI!");
+const searchForm = document.querySelector("#countries-input")
+
+// Function to return country after taking in user input
+const getCountryByName = async (countryName) => {
+  countryName.preventDefault();
+
+    const fetchResponse = await fetch('https://restcountries.com/v3.1/name/' + countryName.target.text.value);
+    console.log(fetchResponse);
+
+    const countriesJSON = await fetchResponse.json();;
+    console.info(countriesJSON[0]);
+
+    const list = document.querySelector("ul");
+    list.textContent = JSON.stringify(countriesJSON[0].name, null, " ");
+    
+
+}
+
+
+searchForm.addEventListener("submit", getCountryByName);
